31a0fb27bb4e10ef9dc103ed90c12f7619257c6f
Henrique Souza <htssouza@gmail.com>
2026-04-08 12:36:58 +0100

    Tests

--- notebooks/module-1/1.5_personal_chef.py
+++ notebooks/module-1/1.5_personal_chef.py
@@ -4,31 +4,42 @@ load_dotenv()
 
 from langchain.tools import tool
 from typing import Dict, Any
-from tavily import TavilyClient
+from tavily import TavilyClient, config
 
 tavily_client = TavilyClient()
 
+
 @tool
 def web_search(query: str) -> Dict[str, Any]:
-
     """Search the web for information"""
 
     return tavily_client.search(query)
 
+
 system_prompt = """
-
 You are a personal chef. The user will give you a list of ingredients they have left over in their house.
-
 Using the web search tool, search the web for recipes that can be made with the ingredients they have.
-
 Return recipe suggestions and eventually the recipe instructions to the user, if requested.
-
 """
 
 from langchain.agents import create_agent
+from langchain.messages import HumanMessage
 
 agent = create_agent(
-    model="gpt-5-nano",
-    tools=[web_search],
-    system_prompt=system_prompt
+    model="gpt-5-nano", tools=[web_search], system_prompt=system_prompt
 )
+
+config = {"configurable": {"thread_id": "1"}}
+
+response = agent.invoke(
+    {
+        "messages": [
+            HumanMessage(
+                content="I have some leftover chicken and rice. What can I make?"
+            )
+        ]
+    },
+    config,
+)
+
+print(response["messages"][-1].content)
